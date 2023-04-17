06e3b00bee089aacb3498958c12c4793f3bd88e1
achaudhary591 <achaudhary591@gmail.com>
2023-04-17 16:28:57 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyHomePage(),
+  );
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.indigo,
+          backgroundColor: Colors.white,
+          accentColor: Colors.amber,
+          errorColor: Colors.orange,
+        ).copyWith(secondary: Colors.indigoAccent),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
