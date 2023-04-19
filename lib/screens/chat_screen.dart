f13873c5aacbed1f85660424282e6623c7fe4e60
achaudhary591 <achaudhary591@gmail.com>
2023-04-19 08:38:52 +0530

    added and fetching text from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -28,25 +29,27 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
         backgroundColor: Colors.indigoAccent,
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('This works!'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/v2soK3e5qTlEEIvgLhP2/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if(streamSnapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: loadingWidget(50),
+            );
+          }
+          return ListView.builder(
+            itemCount: streamSnapshot.data?.docs.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('This works!'),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/v2soK3e5qTlEEIvgLhP2/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((document) {
-              print("message data=====> ${document['text']}");
-            });
-          });
-        },
-        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
