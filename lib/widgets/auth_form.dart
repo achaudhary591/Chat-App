1fd07e49fb8633f5b7bf3022d57b0ef96f0743f2
achaudhary591 <achaudhary591@gmail.com>
2023-04-19 11:55:25 +0530

    auth screen ui

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -13,61 +13,52 @@ class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.3),
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.4),
-              blurRadius: 10,
-            ),
-          ],
-        ),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Card(
-            margin: const EdgeInsets.all(20),
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(16),
-              child: Form(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: 'Email Address',
-                        labelStyle: TextStyle(color: Colors.grey),
-                      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+        child: Card(
+          shadowColor: Colors.grey[800],
+          color: Colors.transparent.withOpacity(0.3),
+          semanticContainer: true,
+          margin: const EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      labelStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Username',
-                        labelStyle: TextStyle(color: Colors.grey),
-                      ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.grey),
-                      ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.grey[300]),
-                      child: const Text('Login'),
-                    ),
-                    TextButton(
-                      child: const Text('Create new Account'),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.grey[300]),
+                    child: const Text('Login'),
+                  ),
+                  TextButton(
+                    child: const Text('Create new Account'),
+                    onPressed: () {},
+                  ),
+                ],
               ),
             ),
           ),
@@ -76,3 +67,4 @@ class _AuthFormState extends State<AuthForm> {
     );
   }
 }
+
