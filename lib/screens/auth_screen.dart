a4ef5485e1e3fc2d991fe875943346a5596fd877
achaudhary591 <achaudhary591@gmail.com>
2023-04-19 17:23:17 +0530

    auth screen ui

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,8 @@
 import 'package:chat_app/widgets/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -9,17 +12,68 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String? userName,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    UserCredential authResult;
+    try {
+      setState(() {
+        _isLoading= true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        await FirebaseFirestore.instance.collection('users').doc(authResult.user!.uid).set({
+          'username': userName,
+          'email': email,
+        });
+      }
+    } on PlatformException catch (e) {
+      // TODO
+      var message = 'An error occurred, please check your credentials!';
+      if (e.message != null) {
+        message = e.message!;
+      }
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      ));
+      setState(() {
+        _isLoading= false;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        _isLoading= false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/img.png'),
-            fit: BoxFit.cover,
-          )
+            image: DecorationImage(
+          image: AssetImage('assets/img.png'),
+          fit: BoxFit.cover,
+        )),
+        child: AuthForm(
+          _submitAuthForm,
+          _isLoading,
         ),
-        child: const AuthForm(),
       ),
     );
   }
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,17 +1,30 @@
+import 'dart:async';
 import 'dart:ui';
 
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({Key? key}) : super(key: key);
+  AuthForm(this.submitFn, this.isLoading);
+
+  final bool isLoading;
+
+  final void Function(
+    String email,
+    String password,
+    String? username,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
 
   @override
   State<AuthForm> createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
-
-  final emailRegex = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  final emailRegex =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
   final _formKey = GlobalKey<FormState>();
 
@@ -20,17 +33,19 @@ class _AuthFormState extends State<AuthForm> {
   var _userName = '';
   var _userPassword = '';
 
-  void _trySubmit(){
+  void _trySubmit() async {
     final isValid = _formKey.currentState?.validate();
     FocusScope.of(context).unfocus();
 
-    if(isValid == true){
+    if (isValid == true) {
       _formKey.currentState!.save();
-      print('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=');
-      print(_userName);
-      print(_userEmail);
-      print(_userPassword);
-      print('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=');
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
     }
   }
 
@@ -52,8 +67,11 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
-                    validator: (String? value){
-                      if(value == null || value.isEmpty || !RegExp(emailRegex).hasMatch(value)){
+                    key: const ValueKey('em@il'),
+                    validator: (String? value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          !RegExp(emailRegex).hasMatch(value)) {
                         return 'Please enter a valid email address';
                       }
                       return null;
@@ -64,30 +82,34 @@ class _AuthFormState extends State<AuthForm> {
                       labelStyle: TextStyle(color: Colors.grey[400]),
                     ),
                     style: const TextStyle(color: Colors.yellow),
-                    onSaved: (value){
+                    onSaved: (value) {
                       _userEmail = value!;
                     },
                   ),
-                  if(!_isLogin)
-                  TextFormField(
-                    validator: (value){
-                      if(value == null || value.length < 4 || value.isEmpty){
-                        return 'Please enter al least 4 characters long username';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                      labelStyle: TextStyle(color: Colors.grey[400]),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: const ValueKey('username'),
+                      validator: (value) {
+                        if (value == null ||
+                            value.length < 4 ||
+                            value.isEmpty) {
+                          return 'Please enter al least 4 characters long username';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(color: Colors.grey[400]),
+                      ),
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
+                      style: const TextStyle(color: Colors.orangeAccent),
                     ),
-                    onSaved: (value){
-                      _userName = value!;
-                    },
-                    style: const TextStyle(color: Colors.orangeAccent),
-                  ),
                   TextFormField(
-                    validator: (value){
-                      if(value == null || value.length < 5 || value.isEmpty){
+                    key: const ValueKey('password'),
+                    validator: (value) {
+                      if (value == null || value.length < 5 || value.isEmpty) {
                         return 'Password must be 5 characters long';
                       }
                       return null;
@@ -97,7 +119,7 @@ class _AuthFormState extends State<AuthForm> {
                       labelText: 'Password',
                       labelStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    onSaved: (value){
+                    onSaved: (value) {
                       _userPassword = value!;
                     },
                     style: const TextStyle(color: Colors.yellow),
@@ -105,26 +127,36 @@ class _AuthFormState extends State<AuthForm> {
                   const SizedBox(
                     height: 12,
                   ),
-                  ElevatedButton(
-                    onPressed: () {_trySubmit();},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: _isLogin ? Colors.blueAccent : Colors.greenAccent),
-                    child: Text(
-                      _isLogin ? 'Login' : 'SignUp',
-                      style: TextStyle(color: _isLogin ? Colors.white : Colors.red),
+                  if (widget.isLoading) loadingWidget(25),
+                  if (!widget.isLoading)
+                    ElevatedButton(
+                      key: const ValueKey('submitButton'),
+                      onPressed: () {
+                        _trySubmit();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            _isLogin ? Colors.blueAccent : Colors.greenAccent,
+                      ),
+                      child: Text(
+                        _isLogin ? 'Login' : 'SignUp',
+                        style: TextStyle(
+                            color: _isLogin ? Colors.white : Colors.red),
+                      ),
                     ),
-                  ),
-                  TextButton(
-                    child: Text(
-                      !_isLogin ? 'Already a member?' : 'Create new Account',
-                      style: const TextStyle(color: Colors.cyan),
+                  if (!widget.isLoading)
+                    TextButton(
+                      key: const ValueKey('login'),
+                      child: Text(
+                        !_isLogin ? 'Already a member?' : 'Create new Account',
+                        style: const TextStyle(color: Colors.cyan),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    },
-                  ),
                 ],
               ),
             ),
